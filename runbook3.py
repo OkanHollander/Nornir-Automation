5ea28f173efbcc19bdb7c4ee93ac31b3e7e584e1
o.hollander <o.hollander1990@gmail.com>
2022-11-02 16:46:33 -0400

    changed to send_commands

--- runbook3.py
+++ runbook3.py
@@ -1,5 +1,6 @@
 from nornir import InitNornir
-from nornir_scrapli.tasks import send_command
+# from nornir_scrapli.tasks import send_command
+from nornir_scrapli.tasks import send_commands
 from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="config.yaml")
@@ -7,7 +8,7 @@ nr = InitNornir(config_file="config.yaml")
 command_list = ['show ip int brief','show version','show ip route']
 def show_command_test(task):
     for cmd in command_list:
-        task.run(task=send_command, command=cmd)
+        task.run(task=send_commands, commands=command_list)
 
 
 results = nr.run(show_command_test)
