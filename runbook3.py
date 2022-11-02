4b8f80ee6ef8bc690b5237826f76bbb441b09dda
o.hollander <o.hollander1990@gmail.com>
2022-11-02 16:44:13 -0400

    Added multiple commands

--- runbook3.py
+++ runbook3.py
@@ -4,9 +4,10 @@ from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="config.yaml")
 
-
+command_list = ['show ip int brief','show version','show ip route']
 def show_command_test(task):
-    task.run(task=send_command, command="show ip int brief")
+    for cmd in command_list:
+        task.run(task=send_command, command=cmd)
 
 
 results = nr.run(show_command_test)
