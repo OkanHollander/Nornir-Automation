2c36ccd137e91c0342d801757e46e32257e5418c
o.hollander <o.hollander1990@gmail.com>
2022-11-03 03:31:44 -0400

    uptime example

--- ipdb_object_inspection.py
+++ ipdb_object_inspection.py
@@ -9,10 +9,12 @@ nr = InitNornir(config_file="config.yaml")
 def pull_structured_data(task):
     version_result = task.run(task=send_command, command="show version")
     task.host["facts"] = version_result.scrapli_response.genie_parse_output()
-    rprint(task.host["facts"])
+    # rprint(task.host["facts"])
+    uptime = task.host["facts"]["version"]["uptime"]
+    rprint(f"{task.host} has an uptime of {uptime}")
 
 
 result = nr.run(pull_structured_data)
 # print_result(result)
 # nr.inventory.hosts[]
-ipdb.set_trace()
+# ipdb.set_trace()
