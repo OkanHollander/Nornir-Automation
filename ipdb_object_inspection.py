d1e92ce6be739f25543b07893c812eedfe66fa1e
o.hollander <o.hollander1990@gmail.com>
2022-11-03 03:20:22 -0400

    Implement genie parser, ipdb

--- ipdb_object_inspection.py
+++ ipdb_object_inspection.py
@@ -1,12 +1,17 @@
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_command
 from nornir_utils.plugins.functions import print_result
+from rich import print as rprint
+import ipdb
 
 nr = InitNornir(config_file="config.yaml")
 
 def pull_structured_data(task):
-    task.run(task=send_command, command="show version")
+    version_result = task.run(task=send_command, command="show version")
+    task.host["facts"] = version_result.scrapli_response.genie_parse_output()
+    rprint(task.host["facts"])
 
 
 result = nr.run(pull_structured_data)
-print_result(result)
+# print_result(result)
+ipdb.set_trace()
