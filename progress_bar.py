0d7fee5579039e4cbc3e2b2b3927b87e93539c8d
o.hollander <o.hollander1990@gmail.com>
2022-11-07 14:11:55 -0500

    Added progress bar config

--- progress_bar.py
+++ progress_bar.py
@@ -3,19 +3,22 @@ from nornir_scrapli.tasks import send_command
 from nornir_utils.plugins.functions import print_result
 from nornir_scrapli.functions import print_structured_result
 from rich import print as rprint
+from tqdm import tqdm
 # import ipdb
 
 nr = InitNornir(config_file="config.yaml")
 
-def random_task(task):
+def random_task(task, progress_bar):
+    progress_bar.update()
     int_output = task.run(task=send_command, command="show ip int brief")
     task.host["facts"] = int_output.scrapli_response.genie_parse_output()
     interface = task.host["facts"]["interface"]
     for k, v in interface.items():      
         if v['status'] != "administratively down":
             rprint(f"{task.host} interface: {k} has ip address: {v['ip_address']}")
-    
-result = nr.run(task=random_task)
+
+with tqdm(total=len(nr.inventory.hosts)) as progress_bar:    
+    result = nr.run(task=random_task, progress_bar=progress_bar)
 # print_structured_result(result, parser="genie")
 # print_result(result)
 # ipdb.set_trace()
