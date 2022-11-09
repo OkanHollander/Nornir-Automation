6282404ce7e93578086fb8221010c0438a7cd840
o.hollander <o.hollander1990@gmail.com>
2022-11-09 15:13:24 -0500

    Added asn peer

--- Restconf/restconf_parsing_requests.py
+++ Restconf/restconf_parsing_requests.py
@@ -2,7 +2,7 @@ import requests
 from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 from nornir.core.task import Result
-import ipdb
+# import ipdb
 
 nr = InitNornir(config_file="config.yaml")
 requests.packages.urllib3.disable_warnings()
@@ -18,8 +18,11 @@ def restconf_test(task):
                             auth=(f"{task.host.username}", f"{task.host.password}"), 
                             verify=False)
     task.host["facts"] = response.json()
+    asn = task.host["facts"]["Cisco-IOS-XE-bgp:neighbor"]["remote-as"]
+    peer = task.host["facts"]["Cisco-IOS-XE-bgp:neighbor"]["id"]
+    print(f"Neighbor {peer} is part of asn: {asn}")
     return Result(host=task.host, result=response.text)
     
 result = nr.run(restconf_test)
 # print_result(result)
-ipdb.set_trace()
+# ipdb.set_trace()
