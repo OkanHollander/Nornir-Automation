7d28a320ced1def469a58d5bb19ec40637b165bb
o.hollander <o.hollander1990@gmail.com>
2022-11-09 04:42:53 -0500

    get_config

---
+++ Netconf/netconf_edit-config.py
@@ -0,0 +1,12 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import netconf_edit_config
+from nornir_scrapli.tasks import netconf_get_config
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+def get_yang(task):
+    task.run(task=netconf_get_config, source="running" ,filter_type="xpath", filter_="/native")
+
+result = nr.run(task=get_yang)
+print_result(result)
