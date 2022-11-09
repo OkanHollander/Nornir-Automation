d3bf3af0a3e8e4b4ce9d57e4facd63c17a34b4bf
o.hollander <o.hollander1990@gmail.com>
2022-11-09 04:29:16 -0500

    loop over xml data

--- Netconf/netconf_xmltodict.py
+++ Netconf/netconf_xmltodict.py
@@ -3,16 +3,21 @@ from nornir_scrapli.tasks import netconf_get
 from nornir_utils.plugins.functions import print_result
 import xmltodict
 from pprint import pprint
-import ipdb
+# import ipdb
 
 nr = InitNornir(config_file="config.yaml")
 
 def get_yang(task):
-    result = task.run(task=netconf_get, filter_type="xpath", filter_="interfaces-state//statistics[in-unicast-pkts > 0]")
+    result = task.run(task=netconf_get, filter_type="xpath", filter_="interfaces-state//statistics/[in-unicast-pkts > 0]")
     output = result.result
     dict_result = xmltodict.parse(output)
     task.host["facts"] = dict_result
+    interfaces = task.host["facts"]["rpc-reply"]["data"]["interfaces-state"]["interface"]
+    # print(f"{interfaces['name']}'s in-unicast-pkts are greater than zero")
+    for intf in interfaces:
+        interface_name = intf['name']
+        print(f"{interface_name}'s in-usicast-pkts are greater than zero")
 
 result = nr.run(task=get_yang)
 # print_result(result)
-ipdb.set_trace()
+# ipdb.set_trace()
