db015b998403e19a6075d476c770853cff6c1fd0
o.hollander <o.hollander1990@gmail.com>
2022-11-09 02:23:44 -0500

    netconf_get with native yang model

--- Netconf/netconf_subtree-filter.py
+++ Netconf/netconf_subtree-filter.py
@@ -1,22 +1,22 @@
 from nornir import InitNornir
 from nornir_scrapli.tasks import netconf_get_config
+from nornir_scrapli.tasks import netconf_get
 from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="config.yaml")
 
 # pyang -f sample-xml-skeleton ietf-interfaces.yang
 filter = """
-  <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
-    <interface>
-      <name/>
-      <description/>
-    </interface>
-  </interfaces>
+<interfaces xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-interfaces-oper">
+</interfaces>
 """
 
 
+# def get_device_config(task):
+#     task.run(task=netconf_get_config, source="running", filter_type="subtree", filter_=filter)
+  
 def get_device_config(task):
-    task.run(task=netconf_get_config, source="running", filter_type="subtree", filter_=filter)
-    
+    task.run(task=netconf_get, filter_type="subtree", filter_=filter)
+  
 results = nr.run(task=get_device_config)
 print_result(results)
