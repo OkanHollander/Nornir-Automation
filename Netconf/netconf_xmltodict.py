cd480a4a6ec2c8e267bf6b32ae4e177ab92e58d5
o.hollander <o.hollander1990@gmail.com>
2022-11-09 04:12:08 -0500

    added ipdb

--- Netconf/netconf_xmltodict.py
+++ Netconf/netconf_xmltodict.py
@@ -3,6 +3,7 @@ from nornir_scrapli.tasks import netconf_get
 from nornir_utils.plugins.functions import print_result
 import xmltodict
 from pprint import pprint
+import ipdb
 
 nr = InitNornir(config_file="config.yaml")
 
@@ -10,7 +11,8 @@ def get_yang(task):
     result = task.run(task=netconf_get, filter_type="xpath", filter_="interfaces-state//statistics[in-unicast-pkts > 0]")
     output = result.result
     dict_result = xmltodict.parse(output)
-    pprint(dict_result)
+    task.host["facts"] = dict_result
 
 result = nr.run(task=get_yang)
 # print_result(result)
+ipdb.set_trace()
