78149367ffdf2adfcff01764231d53479e04b4b7
o.hollander <o.hollander1990@gmail.com>
2022-11-09 05:11:55 -0500

    changed from get to edit-config

--- Netconf/netconf_edit-config.py
+++ Netconf/netconf_edit-config.py
@@ -1,12 +1,14 @@
 from nornir import InitNornir
 from nornir_scrapli.tasks import netconf_edit_config
-from nornir_scrapli.tasks import netconf_get_config
 from nornir_utils.plugins.functions import print_result
+from nornir_jinja2.plugins.tasks import template_file
 
 nr = InitNornir(config_file="config.yaml")
 
-def get_yang(task):
-    task.run(task=netconf_get_config, source="running" ,filter_type="xpath", filter_="/native")
+def configure_ospf(task):
+    template_to_load = task.run(task=template_file, template="ospf.j2", path="templates")
+    configuration = template_to_load.result
+    task.run(task=netconf_edit_config, target="running", config=configuration)
 
-result = nr.run(task=get_yang)
+result = nr.run(task=configure_ospf)
 print_result(result)
