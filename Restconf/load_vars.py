e487db9aebf7129b8b04ad0dae630e5305553884
o.hollander <o.hollander1990@gmail.com>
2022-11-10 02:52:46 -0500

    data to change data with restconf

---
+++ Restconf/load_vars.py
@@ -0,0 +1,35 @@
+import requests
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_utils.plugins.tasks.data import load_yaml
+from nornir.core.task import Result
+
+nr = InitNornir(config_file="config.yaml")
+requests.packages.urllib3.disable_warnings()
+headers = {
+    "Accept": "application/yang-data+json",
+    "Content-Type": "application/yang-data+json"    # because it's a put request
+}
+chained_url = "data/openconfig-acl:acl"
+
+def load_data(task):
+    data = task.run(task=load_yaml, file=f"./host_vars/{task.host}.yaml")
+    task.host["facts"] = data.result
+    
+def configure_stuff(task, chained_url, headers):
+    restconf_url = f"https://{task.host.hostname}:{task.host.port}/restconf/"
+    
+    response = requests.put(url=restconf_url + chained_url,
+                            headers=headers,
+                            auth=(f"{task.host.username}", f"{task.host.password}"),
+                            verify=False,
+                            json=task.host["facts"]["configure_acl"])   # configure_acl is the top key in the yaml file!!!
+    return Result(host=task.host, result=response)
+    
+    
+
+load_results = nr.run(task=load_data)
+print_result(load_results)
+
+configure_results = nr.run(task=configure_stuff, chained_url=chained_url, headers=headers)
+print_result(configure_results)
