f09739872d545b4444706e52dcf85c637987a203
o.hollander <o.hollander1990@gmail.com>
2022-11-10 02:56:23 -0500

    scp config

---
+++ Intent-Based-Scripting/scp.py
@@ -0,0 +1,11 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_configs_from_file
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+def push_scp(task):
+    task.run(task=send_configs_from_file, file="scp.txt")
+
+result = nr.run(task=push_scp)
+print_result(result)
