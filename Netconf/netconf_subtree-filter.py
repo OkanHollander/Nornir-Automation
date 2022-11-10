f5d4c3bbbfcd6084f8d544c12cd7dea29ba9e9a7
o.hollander <o.hollander1990@gmail.com>
2022-11-10 14:48:19 -0500

    extra example

--- Netconf/netconf_subtree-filter.py
+++ Netconf/netconf_subtree-filter.py
@@ -7,16 +7,26 @@ nr = InitNornir(config_file="config.yaml")
 
 # pyang -f sample-xml-skeleton ietf-interfaces.yang
 filter = """
-<interfaces xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-interfaces-oper">
-</interfaces>
+  <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+    <interface>
+      <name/>
+      <description/>
+      <type/>
+      <link-up-down-trap-enable/>
+    </interface>
+  </interfaces>
 """
+# filter2 = """
+# <interfaces xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-interfaces-oper">
+# </interfaces>
+# """
 
 
-# def get_device_config(task):
-#     task.run(task=netconf_get_config, source="running", filter_type="subtree", filter_=filter)
-  
 def get_device_config(task):
-    task.run(task=netconf_get, filter_type="subtree", filter_=filter)
+    task.run(task=netconf_get_config, source="running", filter_type="subtree", filter_=filter)
+  
+# def get_device_config(task):
+#     task.run(task=netconf_get, filter_type="subtree", filter_=filter2)
   
 results = nr.run(task=get_device_config)
 print_result(results)
