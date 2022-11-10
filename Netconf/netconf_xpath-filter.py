067632ab588762eee9854efe35977e5da57680c1
o.hollander <o.hollander1990@gmail.com>
2022-11-10 14:52:25 -0500

    Initial Commit

--- Netconf/netconf_xpath-filter.py
+++ Netconf/netconf_xpath-filter.py
@@ -1,8 +1,15 @@
 from nornir import InitNornir
 from nornir_scrapli.tasks import netconf_get
+from nornir_scrapli.tasks import netconf_get_config
 from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="config.yaml")
+
+# def get_yang(task):
+#     task.run(task=netconf_get_config,
+#              source="running",
+#              filter_type="xpath",
+#              filter_="/native")
 
 def get_yang(task):
     task.run(task=netconf_get, 
