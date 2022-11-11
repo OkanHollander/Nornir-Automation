f3832714c172333e7c4d211c04d68b1bda29c36e
o.hollander <o.hollander1990@gmail.com>
2022-11-11 08:40:37 -0500

    added filter code

--- filtering/basic_filter.py
+++ filtering/basic_filter.py
@@ -6,6 +6,9 @@ nr = InitNornir(config_file="config.yaml")
 
 def test_task(task):
     task.run(task=send_command, command="show ip int brief")
-    
-result = nr.run(task=test_task)
+
+
+usa_targets = nr.filter(country="usa")
+
+result = usa_targets.run(task=test_task)
 print_result(result)
