5bf5e45c25e9fce85a891ae0b66d392f8610aab8
o.hollander <o.hollander1990@gmail.com>
2022-11-11 09:19:05 -0500

    F Object

--- filtering/f_filtering.py
+++ filtering/f_filtering.py
@@ -1,12 +1,13 @@
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_command
-from nornir_utils.plugins.functions import print_result
 from nornir_scrapli.functions import print_structured_result
+from nornir.core.filter import F
 
 nr = InitNornir(config_file="config.yaml")
 
 def test_task(task):
     task.run(task=send_command, command="show version")
 
-result = nr.run(task=test_task)
+region = nr.filter(F(region="east"))
+result = region.run(task=test_task)
 print_structured_result(result, parser="genie")
